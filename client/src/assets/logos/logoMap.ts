c535da621323ef8c31d42383350db9f1f9a03acf
Tayyab Rashad <91985882+Tayyab1O1@users.noreply.github.com>
2026-03-20 05:51:37 +0500

    sync: client/src/assets/logos/logoMap.ts

--- client/src/assets/logos/logoMap.ts
+++ client/src/assets/logos/logoMap.ts
@@ -1,3 +1,11 @@
+import smallWorldMusic from "./Small_World_Music.png";
+import bCurrent from "./BCurrent.png";
+import sakeenahFoundation from "./Sakeenah_Foundation.png";
+import eastEndArts from "./East_End_Arts.png";
+import folkCanada from "./Folk_Canada.png";
+import settlementAid from "./Settlement_Aid_For_Newcomers.png";
+import glenEden from "./Glen_Eden_Youth_Center.png";
+
 export interface OrgLogo {
   name: string;
   initials: string;
@@ -5,16 +13,13 @@ export interface OrgLogo {
 }
 
 const orgLogos: OrgLogo[] = [
-  { name: "Bright Futures Foundation", initials: "BF" },
-  { name: "Urban Youth Alliance", initials: "UY" },
-  { name: "Community Builders Network", initials: "CB" },
-  { name: "Hope & Wellness Initiative", initials: "HW" },
-  { name: "Pathways to Success", initials: "PS" },
-  { name: "Empower Together", initials: "ET" },
-  { name: "NextGen Leaders", initials: "NL" },
-  { name: "Resilient Communities", initials: "RC" },
-  { name: "Bridge to Opportunity", initials: "BO" },
-  { name: "Thrive Collective", initials: "TC" },
+  { name: "Small World Music", initials: "SW", logo: smallWorldMusic },
+  { name: "BCurrent", initials: "BC", logo: bCurrent },
+  { name: "Sakeenah Foundation", initials: "SF", logo: sakeenahFoundation },
+  { name: "East End Arts", initials: "EE", logo: eastEndArts },
+  { name: "Folk Canada", initials: "FC", logo: folkCanada },
+  { name: "Settlement Aid for Newcomers", initials: "SA", logo: settlementAid },
+  { name: "Glen Eden Youth Center", initials: "GE", logo: glenEden },
 ];
 
 export default orgLogos;
